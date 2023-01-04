0c64ef74e3bb28def837f9fd52ecd4ce58fa8a97
ddongseop <dlehdtjq00o@gmail.com>
2023-01-04 17:00:34 +0900

    memory repository

---
+++ src/main/java/hello/subFlix_v2/domain/Entertainment.java
@@ -0,0 +1,32 @@
+package hello.subFlix_v2.domain;
+
+public class Entertainment {
+
+    private Long id;
+    private String title;
+    private String url;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+}
---
+++ src/main/java/hello/subFlix_v2/service/EntertainmentService.java
@@ -0,0 +1,14 @@
+package hello.subFlix_v2.service;
+
+import hello.subFlix_v2.repository.EntertainmentRepository;
+import hello.subFlix_v2.repository.MemoryEntertainmentRepository;
+
+public class EntertainmentService {
+
+    private final EntertainmentRepository entertainmentRepository = new MemoryEntertainmentRepository();
+
+    /**
+     * 엔터테인먼트 등록
+     */
+
+}
