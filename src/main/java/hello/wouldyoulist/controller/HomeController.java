82c167f48fb66d1b43a130509605d3d7cde0faee
ddongseop <dlehdtjq00o@gmail.com>
2023-02-08 13:54:59 +0900

    Chore: Spring React 한번에 빌드

--- src/main/java/hello/wouldyoulist/controller/HomeController.java
+++ src/main/java/hello/wouldyoulist/controller/HomeController.java
@@ -2,12 +2,13 @@ package hello.wouldyoulist.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class HomeController {
 
-    @GetMapping("/")
+    @GetMapping("/main")
     public String home() {
-        return "home";
+        return "main";
     }
 }
