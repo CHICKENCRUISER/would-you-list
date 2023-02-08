bf3f60ad54967b56e08da427cceaa0adc57a515c
ddongseop <dlehdtjq00o@gmail.com>
2023-02-08 12:55:22 +0900

    solve cors error

--- src/main/java/hello/wouldyoulist/WebConfig.java
+++ src/main/java/hello/wouldyoulist/WebConfig.java
@@ -9,7 +9,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:8080", "http://localhost:8081")
+                .allowedOrigins("http://localhost:8080", "http://localhost:8081", "http://localhost:3000", "http://localhost:3001")
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowCredentials(true)
                 .maxAge(3000);
