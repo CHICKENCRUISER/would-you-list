b202a22e5202ad083ea3feac3f43e72047d02d9b
ddongseop <dlehdtjq00o@gmail.com>
2023-02-08 21:24:24 +0900

    Feat: JPA 스키마 자동 생성 및 더미데이터 insert

---
+++ src/main/java/hello/wouldyoulist/InitDb.java
@@ -0,0 +1,49 @@
+package hello.wouldyoulist;
+
+import hello.wouldyoulist.domain.Todo;
+import jakarta.annotation.PostConstruct;
+import jakarta.persistence.EntityManager;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+@Component
+@RequiredArgsConstructor
+public class InitDb {
+
+    private final InitService initService;
+
+    @PostConstruct
+    public void init() {
+        initService.dbInit();
+    }
+
+    @Component
+    @Transactional
+    @RequiredArgsConstructor
+    static class InitService {
+
+        private final EntityManager em;
+
+        public void dbInit() {
+            Todo todo1 = createTodo("이동섭", "바빌론", "2월 5일 오후 7:30", "MOVIE", "어쩌구저쩌구");
+            Todo todo2 = createTodo("이동섭", "풋살", "2월 5일 오후 11:00", "SPORTS", "풋살체련 희망자!");
+            Todo todo3 = createTodo("이동섭", "아바타", "2월 9일 오후 3:30", "MOVIE", "아바타2 보기전");
+            Todo todo4 = createTodo("이동섭", "신과함께", "1월 1일 오전 12:00", "MOVIE", "주지훈 머시쪙");
+            em.persist(todo1);
+            em.persist(todo2);
+            em.persist(todo3);
+            em.persist(todo4);
+        }
+
+        private static Todo createTodo(String user, String name, String date, String category, String content) {
+            Todo todo = new Todo();
+            todo.setUser(user);
+            todo.setName(name);
+            todo.setDate(date);
+            todo.setCategory(category);
+            todo.setContent(content);
+            return todo;
+        }
+    }
+}
