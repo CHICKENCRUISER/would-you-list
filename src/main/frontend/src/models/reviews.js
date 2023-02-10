992479b7e093a57a16d5500db4db061ed3ca5c8f
y-ngm-n <wogitopp@naver.com>
2023-02-10 16:11:57 +0900

    feat : review GET model 생성

--- src/main/frontend/src/models/reviews.js
+++ src/main/frontend/src/models/reviews.js
@@ -4,14 +4,18 @@ import axios from "axios";
 
 const url = "http://localhost:8080";
 
+const getReviews = async () => {
+  const res = await axios.get(`${url}/review`);
+  return res.data;
+}
+
 const createReview = async (review) => {
   const res = await axios.post(`${url}/review/new`, review, {
     headers: {
       "Content-Type": "multipart/form-data"
     }
   });
-  console.log(res.data);
   return res.data;
 }
 
-export { createReview };
+export { getReviews, createReview };
