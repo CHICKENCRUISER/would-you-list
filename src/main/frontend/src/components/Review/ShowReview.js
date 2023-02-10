335679bdcd64b5fb1f62a95e74f82011840bb826
dongsuk <scienceyds@naver.com>
2023-02-11 02:39:47 +0900

    fix : 리뷰 상세 화면으로 이동할때 자동 스크롤 위치 버그 해결

--- src/main/frontend/src/components/Review/ShowReview.js
+++ src/main/frontend/src/components/Review/ShowReview.js
@@ -2,20 +2,33 @@ import reviewDummyData from "./reviewDummyData";
 import ReviewCard from "./ReviewCard";
 import { useEffect } from "react";
 import { useParams } from "react-router-dom";
+import { Button } from "@chakra-ui/react";
+import React, { useRef, useState } from "react";
+
 const ShowReview = () => {
   let { id } = useParams();
+  const [height, setHeight] = useState(0);
+  const reviewCardRef = useRef(null);
+
+  useEffect(() => {
+    setHeight(70 + reviewCardRef.current.offsetHeight * (parseInt(id) - 1));
+  }, []);
 
   const useScrollTo = (xpos, ypos) => {
     useEffect(() => {
       window.scrollTo(xpos, ypos);
-    }, []);
+    }, [height]);
   };
-  useScrollTo(0, 70 + 808 * (id - 1));
+  useScrollTo(0, height);
 
   return (
     <div>
       {reviewDummyData.map((data) => {
-        return <ReviewCard data={data} key={data.todoId} />;
+        return (
+          <div ref={reviewCardRef} key={data.todoId}>
+            <ReviewCard data={data} />
+          </div>
+        );
       })}
     </div>
   );
