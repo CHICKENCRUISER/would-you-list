8cade71c9ea063729f4061a3d3e777be70dbe82f
pminsung12 <pminsung12@gmail.com>
2023-02-13 00:50:49 +0900

    [TEST]: s3 .gitignore test

--- src/main/java/hello/wouldyoulist/controller/ReviewController.java
+++ src/main/java/hello/wouldyoulist/controller/ReviewController.java
@@ -2,18 +2,15 @@ package hello.wouldyoulist.controller;
 
 import hello.wouldyoulist.domain.UploadFile;
 import hello.wouldyoulist.domain.Review;
-import hello.wouldyoulist.service.FileService;
-import hello.wouldyoulist.service.ReviewService;
-import hello.wouldyoulist.service.TodoService;
+import hello.wouldyoulist.service.*;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.Data;
-import lombok.extern.slf4j.Slf4j;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.File;
 import java.io.IOException;
 import java.util.List;
 
@@ -24,10 +21,15 @@ public class ReviewController {
     private final FileService fileService;
     private final ReviewService reviewService;
 
-    public ReviewController(TodoService todoService, FileService fileService, ReviewService reviewService) {
+
+    private final S3Uploader s3Uploader;
+
+
+    public ReviewController(TodoService todoService, FileService fileService, ReviewService reviewService, S3Uploader s3Uploader) {
         this.todoService = todoService;
         this.fileService = fileService;
         this.reviewService = reviewService;
+        this.s3Uploader = s3Uploader;
     }
 
     @GetMapping("/review")
@@ -42,7 +44,7 @@ public class ReviewController {
     //참고 링크: https://velog.io/@dhk22/ToyProject-1-SpringBoot를-이용한-파일-업로드에-JPA적용-시키기
     @PostMapping(value = "/review/new")
     @ResponseBody
-    public CreateReviewResponse create(HttpServletRequest request, @RequestParam MultipartFile file) throws IOException {
+    public Long create(HttpServletRequest request, @RequestParam MultipartFile file) throws IOException {
 
         Review review = new Review();
         Long todoId = Long.parseLong(request.getParameter("todoId"));
@@ -57,17 +59,23 @@ public class ReviewController {
         if (file.isEmpty()) {
             review.setPhotoId(0L); //사진 업로드가 안됐을 경우 기본 사진 id로 세팅
         } else {
-            //파일을 지정된 경로에 저장 (실제 물리적 저장)
+//            //파일을 지정된 경로에 저장 (실제 물리적 저장)
             String originalFilename = file.getOriginalFilename();
-            String fullPath = uploadDir + originalFilename;
-            file.transferTo(new File(fullPath));
+//            String fullPath = uploadDir + originalFilename;
+//            file.transferTo(new File(fullPath));
+//
+//            //파일과 리뷰의 정보를 DB에 저장 (논리적 저장)
+//            Long fileId = fileService.save(new UploadFile(originalFilename, fullPath));
+//            review.setPhotoId(fileId);
 
-            //파일과 리뷰의 정보를 DB에 저장 (논리적 저장)
-            Long fileId = fileService.save(new UploadFile(originalFilename, fullPath));
+            String storedFileName=s3Uploader.upload(file,"images");
+
+            Long fileId = fileService.save(new UploadFile(originalFilename, storedFileName));
             review.setPhotoId(fileId);
+
         }
         Long id = reviewService.save(review);
-        return new CreateReviewResponse(id);
+        return id;
     }
 
     @Data
@@ -82,13 +90,13 @@ public class ReviewController {
         private MultipartFile file;
     }
 
-    @Data
-    static class CreateReviewResponse {
-        private Long id;
-
-        public CreateReviewResponse(Long id) {
-            this.id = id;
-        }
-    }
+//    @Data
+//    static class CreateReviewResponse {
+//        private Long id;
+//
+//        public CreateReviewResponse(Long id) {
+//            this.id = id;
+//        }
+//    }
 
 }
