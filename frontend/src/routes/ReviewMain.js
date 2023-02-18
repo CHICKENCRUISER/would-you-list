570f0f58ea19c0dba16d3ad70e516b3185f74f5b
dongsuk <scienceyds@naver.com>
2023-02-19 00:35:11 +0900

    [Fix] 리뷰 미리보기 창에서 세부사항으로 이동할때 순서 안 맞는거 수정

--- frontend/src/routes/ReviewMain.js
+++ frontend/src/routes/ReviewMain.js
@@ -4,11 +4,9 @@ import { getReviews } from "../models/reviews";
 import { SimpleGrid, Card, CardBody, Image } from "@chakra-ui/react";
 import { useNavigate } from "react-router-dom";
 
-
 //그리드 형식으로 리뷰를 보여주는 컴포넌트
 //화면 너비에 따라서 카드의 갯수가 달라짐
 const ReviewMain = ({ reviewToggleEdit }) => {
-
   const [reviews, setReviews] = useState([]);
 
   let navigate = useNavigate();
@@ -17,7 +15,7 @@ const ReviewMain = ({ reviewToggleEdit }) => {
     const f = async () => {
       const res = await getReviews();
       setReviews(res);
-    }
+    };
     f();
   }, []);
 
@@ -29,18 +27,25 @@ const ReviewMain = ({ reviewToggleEdit }) => {
           spacing={4}
           templateColumns="repeat(auto-fill, minmax(150px, 1fr))"
         >
-      {/* reviewDummyData 를 진짜 데이터로 바꿔야함 */}
+          {/* reviewDummyData 를 진짜 데이터로 바꿔야함 */}
           {reviews.map((review) => (
             <Card
-            // 카드 클릭하면 ShowReview로 이동
-            // data.todoId는 입맛에 맛게 바꿔야함
-            //아마 총 리뷰의 개수를 구한다음애 몇번째 리뷰인지를 나타내는 숫자를 보내야할듯
+              // 카드 클릭하면 ShowReview로 이동
+              // data.todoId는 입맛에 맛게 바꿔야함
+              //아마 총 리뷰의 개수를 구한다음애 몇번째 리뷰인지를 나타내는 숫자를 보내야할듯
               key={review.todo.id}
-              onClick={() => { navigate("/review/" + review.todo.id); }}
+              onClick={() => {
+                navigate("/review/" + review.id);
+              }}
               style={{ width: "150px", height: "150px" }}
             >
               <CardBody style={{ padding: "0", justifyContent: "center" }}>
-                <Image objectFit="cover" src={review.photo} width="150px" height="150px" />
+                <Image
+                  objectFit="cover"
+                  src={review.photo}
+                  width="150px"
+                  height="150px"
+                />
               </CardBody>
             </Card>
           ))}
