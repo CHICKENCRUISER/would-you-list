316ad66fbbe7aa9dc92186f8df1a642434cca8f5
pminsung12 <pminsung12@gmail.com>
2023-03-04 16:14:49 +0900

    [FEAT] H2->MYSQL 이사완료, user_account 테이블 생성 쿼리문 추가

--- backend/src/main/java/hello/wouldyoulist/controller/userController.java
+++ backend/src/main/java/hello/wouldyoulist/controller/userController.java
@@ -1,37 +1,37 @@
-package hello.wouldyoulist.controller;
-
-import hello.wouldyoulist.domain.User;
-import hello.wouldyoulist.repository.UserRepository;
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Controller;
-import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-
-//import javax.validation.Valid;
-
-
-@Controller
-@RequiredArgsConstructor
-@RequestMapping("/members")
-public class userController {
-    private final UserRepository userRepository;
-//    @GetMapping("/add")
-//    public String addForm(@ModelAttribute("member") Member member) {
-//        return "members/addMemberForm";
+//package hello.wouldyoulist.controller;
+//
+//import hello.wouldyoulist.domain.User;
+//import hello.wouldyoulist.repository.UserRepository;
+//import lombok.RequiredArgsConstructor;
+//import org.springframework.stereotype.Controller;
+//import org.springframework.validation.BindingResult;
+//import org.springframework.web.bind.annotation.ModelAttribute;
+//import org.springframework.web.bind.annotation.PostMapping;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//
+////import javax.validation.Valid;
+//
+//
+//@Controller
+//@RequiredArgsConstructor
+//@RequestMapping("/members")
+//public class userController {
+//    private final UserRepository userRepository;
+////    @GetMapping("/add")
+////    public String addForm(@ModelAttribute("member") Member member) {
+////        return "members/addMemberForm";
+////    }
+//    @PostMapping("/add")
+//    public String save(@ModelAttribute User user, BindingResult
+//            result) {
+//        if (result.hasErrors()) {
+//            return "members/addMemberForm";
+//        }
+//        userRepository.save(user);
+//        return "redirect:/";
 //    }
-    @PostMapping("/add")
-    public String save(@ModelAttribute User user, BindingResult
-            result) {
-        if (result.hasErrors()) {
-            return "members/addMemberForm";
-        }
-        userRepository.save(user);
-        return "redirect:/";
-    }
-}
-
-
-
-
+//}
+//
+//
+//
+//
