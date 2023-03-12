92872ffa2c9a71af880e80b14e906798ab5ad724
dongsuk <scienceyds@naver.com>
2023-03-12 13:43:58 +0900

    [fix] 변수명 오류 수정(리뷰 추가시 제목 안보이는 문제 해결)

--- frontend/src/routes/AddEditReview.js
+++ frontend/src/routes/AddEditReview.js
@@ -22,10 +22,8 @@ import ReviewAddModal from "../components/Review/ReviewAddModal";
 
 import { createReview, updateReview } from "../models/reviews";
 
-
 // 리뷰 추가와 리뷰 수정을 처리하는 컴포넌트
 const AddEditReview = () => {
-
   // 기본 이미지 url
   const defaultImg =
     "https://wouldyoulistfile.s3.ap-northeast-2.amazonaws.com/images/97c08004-fb34-4fb1-ad4c-4100524d3957defaultPhoto.jpeg";
@@ -33,7 +31,9 @@ const AddEditReview = () => {
   // 상위 컴포넌트에서 useNavigate로 넘겨받은 데이터
   // -> 리뷰 추가이면 빈 객체(단, todo데이터는 들어있음), 리뷰 수정이면 수정할 리뷰 데이터
   const location = useLocation();
-  const { state: { data } } = location;
+  const {
+    state: { data },
+  } = location;
 
   // 리뷰 생성인지 수정인지 구분하는 변수
   // -> 리뷰 추가이면 false, 리뷰 수정이면 true
@@ -101,25 +101,24 @@ const AddEditReview = () => {
   return (
     <Box m={4}>
       <Card>
-
         {/* Header */}
         <CardHeader>
           <Heading size="md" textAlign={"center"}>
-            "{data.todo.name}"에 대한 멋진 리뷰를 남겨 주세요!
+            "{data.todo.todoName}"에 대한 멋진 리뷰를 남겨 주세요!
+            {console.log(data)}
           </Heading>
         </CardHeader>
 
         {/* Body */}
         <CardBody>
           <Stack divider={<StackDivider />} spacing="4">
-
             {/* Todo 정보 */}
             <Box>
               <Heading size="xs" textTransform="uppercase">
                 TODO
               </Heading>
               <Text pt="2" fontSize="sm">
-                {data.todo.name}
+                {data.todo.todoName}
               </Text>
             </Box>
             <Box>
@@ -141,7 +140,6 @@ const AddEditReview = () => {
               </Heading>
               <form onSubmit={reviewFormSubmitted}>
                 <FormControl mt={4}>
-
                   {/* 제목/장소/내용 */}
                   <Input
                     type="text"
@@ -224,14 +222,11 @@ const AddEditReview = () => {
                       }}
                     />
                   </Stack>
-
                 </FormControl>
               </form>
             </Box>
-
           </Stack>
         </CardBody>
-
       </Card>
     </Box>
   );
